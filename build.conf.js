91b609afbe0361f3db9e9c74b90d5ce74a57620e
Aleksandr Koshevierov <iworb.ne@gmail.com>
2020-02-18 10:10:10 +0300

    Configurable

--- build.conf.js
+++ build.conf.js
@@ -22,7 +22,8 @@ module.exports = {
             '../node_modules/pip-suite-all/dist/**/*',
             '../node_modules/pip-admin-system/dist/**/*',
             '../iqs-libs-clientshell-angular/dist/**/*',
-            '../iqs-clients-unsupported-page/**/*'
+            '../iqs-clients-unsupported-page/**/*',
+            './src/config.js'
         ]
     },
     samples: {
--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,9 @@
 (() => {
-    function iqsPositronConfigConfig(
-        pipAuthStateProvider: pip.rest.IAuthStateProvider,
-        pipErrorPageConfigServiceProvider: pip.errors.IErrorPageConfigProvider,
-    ) {
-        pipAuthStateProvider.authorizedState = 'app.users';
-        pipErrorPageConfigServiceProvider.configs.NoConnection.RedirectSateDefault = pipAuthStateProvider.authorizedState;
-
-    }
-
     angular
         .module('iqsPositronConfig.Config', [
             'ngCookies',
             'iqsShell',
             'pipSystem',
             'pipSystem.Templates',
-        ])
-        .config(iqsPositronConfigConfig);
+        ]);
 })();
